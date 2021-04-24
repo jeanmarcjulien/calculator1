e53d97eb188512490363b95125372374c4681fc3
jeanmarcjulien <julienjeanmarc@gmail.com>
2021-04-24 09:23:43 -0500

    last commit

--- src/test/java/com/galvanize/CalculatorTestCase.java
+++ src/test/java/com/galvanize/CalculatorTestCase.java
@@ -22,8 +22,75 @@ public class CalculatorTestCase {
 
         //teardown
     }
-}
 
-//comment1
-//comment2
+    @Test
+    void testAddTwoNumbers() {
+
+        //setup
+        Calculator calculator;
+        calculator = new Calculator();
+
+        //execution
+        int expected=5;
+        int actual = calculator.addInputs(2,3);
+
+        //assertion
+        assertEquals(expected, actual);
+
+        //teardown
+    }
+
+    @Test
+    void testAddManyNumbers() {
+
+        //setup
+        Calculator calculator;
+        calculator = new Calculator();
+
+        //execution
+        int expected=9;
+        int actual = calculator.addInputs(1,3,5);
+
+        //assertion
+        assertEquals(expected, actual);
+
+        //teardown
+    }
+
+    @Test
+    void testMultiplyTwoNumbers() {
+
+        //setup
+        Calculator calculator;
+        calculator = new Calculator();
+
+        //execution
+        int expected=15;
+        int actual = calculator.multiplyInputs(3,5);
+
+        //assertion
+        assertEquals(expected, actual);
+
+        //teardown
+    }
+
+    @Test
+    void testMultiplyManyNumbers() {
+
+        //setup
+        Calculator calculator;
+        calculator = new Calculator();
+
+        //execution
+        int expected=48;
+        int actual = calculator.multiplyInputs(2,4,6);
+
+        //assertion
+        assertEquals(expected, actual);
+
+        //teardown
+    }
+
+
+}
 
