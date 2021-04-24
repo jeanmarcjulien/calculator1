729d9ce348b0e7879bbabfc1497679ed30d91c48
jeanmarcjulien <julienjeanmarc@gmail.com>
2021-04-24 09:50:27 -0500

    initial

--- src/main/java/com/galvanize/Calculator.java
+++ src/main/java/com/galvanize/Calculator.java
@@ -2,31 +2,4 @@ package com.galvanize;
 
 public class Calculator {
 
-    private int value=0;
-
-    public int getValue() {
-        return value;
-    }
-
-    public int addInputs(int... numbers) {
-
-        int sum =0;
-
-        for(int i =0;i<numbers.length;i++){
-            sum +=numbers[i];
-        }
-        value=sum;
-        return value;
-    }
-
-    public int multiplyInputs(int... numbers) {
-
-        int product =1;
-
-        for(int i =0;i<numbers.length;i++){
-            product *=numbers[i];
-        }
-        value=product;
-        return value;
-    }
 }
